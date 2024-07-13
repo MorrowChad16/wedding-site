f4f145a2ca4751cd6eeaed42fe40bc04f4c76d27
Chad Morrow <morrvcha@amazon.com>
2024-07-13 10:59:46 -0600

    Add official timeline

--- src/pages/OurStory.tsx
+++ src/pages/OurStory.tsx
@@ -9,26 +9,51 @@ import {
     TimelineOppositeContent,
 } from '@mui/lab';
 import { Box, Card, CardMedia } from '@mui/material';
-import potatoes from '../assets/images/potatoes.jpeg';
-import veggie from '../assets/images/veggie.jpeg';
-import dessert from '../assets/images/dessert.jpeg';
+import taco_tuesday from '../assets/images/story/taco_tuesday.jpeg';
+import roadtrip from '../assets/images/story/roadtrip.jpeg';
+import chicago from '../assets/images/story/chicago.jpeg';
+import russia from '../assets/images/story/russia.jpeg';
+import boise from '../assets/images/story/boise.jpeg';
+import mallorca from '../assets/images/story/mallorca.jpeg';
+import engagement from '../assets/images/story/engagement.jpeg';
+
 
 export default function OurStory() {
     const timelineData = [
         {
-            title: 'Event 1',
-            description: 'This is the description for the first event.',
-            image: potatoes,
+            title: 'Taco Tuesday',
+            description: 'Our story began at Iowa State during the onset of COVID. Chad\'s taco night for friends unexpectedly became the recipe for romance. For Ciara, it was love at first bite.',
+            image: taco_tuesday,
         },
         {
-            title: 'Event 2',
-            description: 'This is the description for the second event.',
-            image: veggie,
+            title: 'Road Trip! w/Mom',
+            description: 'In a bold move, Chad introduced Ciara to his mom right before whisking her away on a three-week cross-country adventure. Surprisingly, this whirlwind start didn\'t end in disaster—it was just the beginning of their journey together.',
+            image: roadtrip,
         },
         {
-            title: 'Event 3',
-            description: 'This is the description for the third event.',
-            image: dessert,
+            title: 'Chicago Bound',
+            description: 'We made our first big move together, venturing out of state to Oak Park.',
+            image: chicago,
+        },
+        {
+            title: 'Russian Cosmonauts',
+            description: 'After COVID restrictions eased, we made our first trip to see Ciara\'s family in Russia. Vodka was plentiful.',
+            image: russia,
+        },
+        {
+            title: 'Boise Bound',
+            description: 'Idaho? no you da hoe. Ciara\'s job brought us out to Boise and we haven\'t looked back since.',
+            image: boise,
+        },
+        {
+            title: 'Fuimos a España',
+            description: 'Following our recent move from Russia, we made our first trip to visit Ciara\'s family in Mallorca.',
+            image: mallorca,
+        },
+        {
+            title: 'Engagement',
+            description: 'Oceanside dinner in an old Spanish fort, her family waiting on a boat? It was the perfect ending to our dating lives.',
+            image: engagement,
         },
     ];
 
