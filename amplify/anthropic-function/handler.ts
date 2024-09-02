2721e46c792630a1fb19617c9041691e139ec34b
Chad Morrow <morrvcha@amazon.com>
2024-09-01 19:42:23 -0600

    Add more logs again

--- amplify/anthropic-function/handler.ts
+++ amplify/anthropic-function/handler.ts
@@ -9,10 +9,12 @@ const anthropic = new Anthropic({
 export const handler: Schema['askWeddingQuestion']['functionHandler'] = async (event) => {
     try {
         const { context, question } = event.arguments;
+        console.log('Context: ', context);
+        console.log('Questions: ', question)
 
         // Call the Anthropic API
         const msg = await anthropic.messages.create({
-            model: 'claude-3-haiku-20240307',
+            model: 'claude-3-sonnet-20240229',
             max_tokens: 300,
             temperature: 0.5,
             system: context!,
@@ -29,7 +31,7 @@ export const handler: Schema['askWeddingQuestion']['functionHandler'] = async (e
             ],
         });
 
-        console.log(msg.content);
+        console.log('Content List: ', msg.content);
         return (msg.content[0] as TextBlock).text;
     } catch (error) {
         return "I'm sorry, I encountered an error while processing your question. Please try again later or contact the wedding organizer for assistance.";
