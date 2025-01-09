434f0804b07ebdae3c4f15176d08fb4b89dba8d8
Chad Morrow <morrvcha@amazon.com>
2025-01-08 18:42:45 -0700

    Temp logging to debug

--- src/api/use-guests.tsx
+++ src/api/use-guests.tsx
@@ -108,6 +108,9 @@ export const hasSubmittedRsvp = (email: string) => {
                 },
             });
 
+            const tempResponse = await getClient().models.Guest.list();
+            console.log(tempResponse);
+
             return response.data.some((guest) => guest.status !== Status.ATTENDING);
         },
         retry: 3,
