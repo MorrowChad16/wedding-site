840e5efc18dbf9bfd579c762b69563780f7187c1
Chad Morrow <morrvcha@amazon.com>
2025-01-09 20:07:59 -0700

    reverse the logic

--- src/components/chatbot.tsx
+++ src/components/chatbot.tsx
@@ -283,7 +283,7 @@ function ChatBot() {
 
     return (
         <div>
-            {location.pathname.includes('rsvp') && (
+            {!location.pathname.includes('rsvp') && (
                 <Fab
                     color="primary"
                     variant="extended"
