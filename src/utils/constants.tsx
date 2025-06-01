7aba46c6fab7888b838855c7fcf629c1167e32a6
Chad Morrow <morrvcha@amazon.com>
2025-06-01 13:29:16 -0500

    update countdown clock. update honeymoon image

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,3 +1,3 @@
-export const WEDDING_DATE = new Date(1749931200000);
+export const WEDDING_DATE = new Date(1749940200000);
 export const PAST_DUE_DATE = WEDDING_DATE < new Date();
 export const COUPLE_NAMES = 'Ciara and Chad' as const;
