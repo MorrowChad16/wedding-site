5354a1589ffd92248e22ee45ee255ba660bda603
Chad Morrow <morrvcha@amazon.com>
2025-07-13 21:27:30 -0600

    add storage backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,10 +3,12 @@ import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { anthropicFunction } from './anthropic-function/resource';
 import { adminAuthFunction } from './admin-auth-function/resource';
+import { storage } from './storage/resource';
 
 defineBackend({
     auth,
     data,
     anthropicFunction,
     adminAuthFunction,
+    storage
 });
